d0fcbfa4b0cf99e2397082e42b4dc23ff26ab074
zeze.li <lizeze.boy@gmail.com>
2019-06-14 11:18:54 +0800

    增加下载、删除、颜色

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 let ctx = canvas.getContext('2d');
 
 let lastPoint = {x: undefined, y: undefined};
-let draw = false;
+let draw = true;
 let eraserEnable = false;
+let using = false;
 
 function resize() {
     canvas.width = document.documentElement.clientWidth;
     canvas.height = document.documentElement.clientHeight;
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
+setColors();
 listenToUser();
 resize();
+clickColor();
 window.onresize = function () {
     resize();
+}
+
+brush.onclick = function () {
+    $(this).addClass('active').siblings().removeClass('active');
+    draw = true;
+    eraserEnable = false;
+}
+earser.onclick = function () {
+    $(this).addClass('active').siblings().removeClass('active');
+    draw = false;
+    eraserEnable = true;
 }
 
 
@@ -21,15 +37,15 @@ function listenToUser() {
     if (document.body.ontouchstart !== undefined) {
         canvas.ontouchstart = function (e) {
             lastPoint = {x: e.touches[0].clientX, y: e.touches[0].clientY};
-            draw = true;
+            using = true;
         }
         canvas.ontouchmove = function (e) {
 
             let x = e.touches[0].clientX;
             let y = e.touches[0].clientY;
+            if (!using) return;
             if (eraserEnable) {
-
-                ctx.clearRect(x, y, 30, 30)
+                clearRect(x, y);
             } else {
                 if (!draw) return;
                 createLine(x, y);
@@ -37,15 +53,16 @@ function listenToUser() {
             }
         }
         canvas.ontouchend = function (e) {
-            draw = false
+            using = false;
         }
     } else {
         canvas.onmousemove = function (e) {
             let x = e.clientX;
             let y = e.clientY;
+            if (!using) return;
             if (eraserEnable) {
+                clearRect(x, y)
 
-                ctx.clearRect(x, y, 30, 30)
             } else {
                 if (!draw) return;
                 createLine(x, y);
@@ -58,26 +75,78 @@ function listenToUser() {
             let x = e.clientX;
             let y = e.clientY;
             lastPoint = {x: x, y: y};
-            draw = true;
+            using = true;
 
         }
         canvas.onmouseup = function (e) {
-
-            draw = false
+            using = false
         }
-    }
-    eraser.onclick = function () {
-        eraserEnable = !eraserEnable;
     }
 
     function createLine(x, y) {
         ctx.beginPath();
         ctx.lineCap = "round";
-        ctx.strokeStyle = '#fff';
         ctx.moveTo(lastPoint.x, lastPoint.y);
         ctx.lineWidth = 10;
         ctx.lineTo(x, y);
         ctx.stroke();
 
     }
+}
+
+function clearRect(x, y, w, h) {
+    ctx.clearRect(x, y, w ? w : 30, h ? h : 30)
+}
+
+
+function setColors() {
+    let $colors = document.getElementById('colors')
+    let colors = ['#000000', 'red', 'orange', 'yellow', 'blue', 'green', 'gray'];
+    for (let i = 0; i < colors.length; i++) {
+        let element = colors[i];
+        let $li = addColorElement(element, i);
+        $colors.append($li);
+    }
+}
+
+function addColorElement(element, index) {
+    let $li = document.createElement('li');
+    $li.setAttribute('color', element);
+    let $div = document.createElement('div');
+
+    if (index === 0) {
+        $div.classList.add('active');
+    }
+    $div.style.background = element
+    $li.append($div);
+    return $li;
+}
+
+
+function clickColor() {
+    $('#colors li').click(function (e) {
+
+        let color = e.currentTarget.getAttribute('color');
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+        $(this).addClass('active').siblings().removeClass('active');
+        $('#brush').click()
+    })
+
+}
+
+download.onclick = function () {
+    let url = canvas.toDataURL();
+    let $a = document.createElement('a')
+    $a.href = url;
+    $a.download = 'aaa'
+    $a.target = '_blank'
+    document.body.append($a);
+    $a.click()
+
+}
+
+clear.onclick = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
 }
